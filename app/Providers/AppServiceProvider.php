e321d688acca8794efdbb808b1c309d1beee5166
medyoucef <oulladimohamedyoucef10@gmail.com>
2026-05-29 21:18:09 -0400

    mettre à ajour le CD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,10 +9,18 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    public function register()
+{
+    if (env('TELESCOPE_ENABLED', true) === false) {
+        return;
     }
+
+    if ($this->app->environment('local')) {
+        $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        $this->app->register(\Laravel\Telescope\TelescopeApplicationServiceProvider::class);
+    }
+}
+
 
     /**
      * Bootstrap any application services.
